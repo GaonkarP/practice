2420b512ee0225d9c2be8f7718afb5d49ae26f52
Praveenkumar Gaonkar <pksgaon@gmail.com>
2020-09-23 19:06:53 +0530

    initial commit with py codes

--- python_scripts/helper.py
+++ python_scripts/helper.py
@@ -11,17 +11,70 @@ def cretelist(num):
     li2.insert(50, -10)
     print(li2)
 
-def listsorting():    
-    print("list sorting")
+def fibonaci_series(length = 0, maxnum = 0):
+    if length != 0 or maxnum != 0 :
+        a = 0
+        b = 1
+        total = sum([a,b])
+        print(f"first total{total}")
+        fib_ser = []
+        fib_ser.append(a)
+        fib_ser.append(b)
+        if length != 0:
+            for i in range(2, length):
+                total = sum([a,b])
+                fib_ser.append(total)
+                a = b
+                b = total
+
+        elif maxnum != 0:
+            while(total<= maxnum):
+                a = b
+                b = total
+                fib_ser.append(total)
+                total = sum([a,b])
+
+            
+
+        print(f"fibonacci series is {fib_ser}")
+    else:
+        print("please provide the range/max number")
+
+def myprint(func):
+    func()
+
+
+def printerror():
+    print(f"[ERROR]:    ", end="")
+
+    
+    print(f"<---")
+
+def number_sorting(num = 0):
+    ''' Hi
+    just checking    
+    '''
+    print(__file__)
+    if num == 0:
+        num = 10
 
     li1=[]
     li1 = [1, 4, 66, -9, -39, -88, 99, 66, 6, 55]
-
     print("enter input numbers")
-    # for i in range(0,10):
-    #     li1.append(int(input()))
+
+    while len(li1) < num:
+        try:
+            li1.append(int(input()))
+        
+        except ValueError as err:
+            print(f"error: {err}")
+        except KeyboardInterrupt:
+            print("exiting as there is a keyboard interrupt")
+            sys.exit(-1)
     print(f"given numbers {li1}")
 
     li1.sort(reverse=True)
+    li1 = sorted(li1, reverse=True)
+
     print(f"sorted numbers are -->{li1}")
-    print(f"second least number is {li1[(len(li1) - 1)]}")
+    print(f"second least number is {li1[(len(li1) - 2)]}")
---
+++ python_scripts/mytest.py
@@ -0,0 +1,55 @@
+
+import os
+import shutil
+import sys
+import getpass
+
+import helper as hp
+
+
+
+def changevalue(var1):
+    var1 = 10
+
+    return var1
+
+def my_function_with_args(username, greeting):
+    print("Hello, %s , From My Function!, I wish you %s"%(username, greeting))
+
+class Bclass():
+
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+    def myprint(self):
+        print(f"input values name is {self.name}, corresponding value is {self.value}")
+    
+
+class C(Bclass):
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+    def myprintC(self):
+        print(f"input values from C clsass: name is {self.name}, corresponding value is {self.value}")
+    
+class D:
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+    def myprintD(self):
+        print(f"input values from D clsass: name is {self.name}, corresponding value is {self.value}")
+
+def main():
+    cwd = os.getcwd()
+    hp.number_sorting()
+
+    # hp.cretelist(100)
+    # hp.fibonaci_series(maxnum= 8973978734)
+    # print(hp.myprint)
+
+
+
+    # test = hp.myprint(hp.printerror)
+
+
+exit(main())
