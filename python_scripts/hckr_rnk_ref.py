2503040bef9241581e8546d4a8272f6b79761bc2
Praveenkumar Gaonkar <47197758+GaonkarP@users.noreply.github.com>
2022-07-03 16:43:38 +0530

    Update hckr_rnk_ref.py

--- python_scripts/hckr_rnk_ref.py
+++ python_scripts/hckr_rnk_ref.py
@@ -1,5 +1,9 @@
 import sys
 import os
+import logging
+
+
+VERSION = '1.3.3'
 
 #find script location
 if getattr(sys, "frozen", False):
@@ -8,3 +12,55 @@ if getattr(sys, "frozen", False):
     current_dir = os.path.dirname(sys.executable)
 else:
     current_dir = os.path.dirname(os.path.abspath(__file__))
+    
+def configLogger(logfile='Logfile.log'):
+    # create logger
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    # create file handler which logs even debug messages
+    fh = logging.FileHandler(logfile, mode="w")
+    fh.setLevel(logging.INFO)
+    # create console handler with a higher log level
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+    # create formatter and add it to the handlers
+    formatter = logging.Formatter('%(levelname)s - %(message)s')
+    # formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
+    ch.setFormatter(formatter)
+    # add the handlers to the logger
+    logger.addHandler(fh)
+    logger.addHandler(ch)
+
+    return logger
+    
+
+def get_args():
+    desc = 'argument parser'
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument('--infile',   required='--support' not in sys.argv and '--version' not in sys.argv,
+                        help='input file path')
+    parser.add_argument('--hash', default="sha256", required=False,
+                        help='hash calc')
+    parser.add_argument('--version', action="store_true", required=False,
+                        help='script version')
+    return parser.parse_args()
+
+def file_operation(outfile):
+    
+    with open(outfile, 'w') as writefile:
+        writefile.write("checkinng file operation")
+    if os.path.isfile(outfile):
+        log.info(f"file created")
+
+
+def main():
+#     args = get_args(sys.argv[1:])
+    args = get_args()
+    
+    if args.version:
+        log.info(f"VERSION - {VERSION}")
+        return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
