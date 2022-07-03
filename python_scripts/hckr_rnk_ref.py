24c32e034860170c7cb4c8da98fb79f8024c8f0a
Praveenkumar Gaonkar <47197758+GaonkarP@users.noreply.github.com>
2022-07-03 16:52:15 +0530

    Update hckr_rnk_ref.py

--- python_scripts/hckr_rnk_ref.py
+++ python_scripts/hckr_rnk_ref.py
@@ -40,11 +40,27 @@ def get_args():
     parser = argparse.ArgumentParser(description=desc)
     parser.add_argument('--infile',   required='--support' not in sys.argv and '--version' not in sys.argv,
                         help='input file path')
-    parser.add_argument('--hash', default="sha256", required=False,
-                        help='hash calc')
+    parser.add_argument('--varb', default="prav", required=False,
+                        help='var b')
     parser.add_argument('--version', action="store_true", required=False,
                         help='script version')
     return parser.parse_args()
+
+
+def get_file_args(argum):
+    '''
+    you can also pass general arguments may be read from file? etc
+    it hould be list of arguments as a list
+    works as fine as system argument
+    '''
+    desc = 'update the hexfile'
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument('-a', required=True, 
+                        choices=["var1", "var2"],
+                        help='choice param')
+    parser.add_argument('-b', required=True,
+                        help='bar b')
+    return parser.parse_args(argum)
 
 def file_operation(outfile):
     
@@ -53,6 +69,13 @@ def file_operation(outfile):
     if os.path.isfile(outfile):
         log.info(f"file created")
 
+def checkfile(filename):
+    if os.path.isfile(filename):
+        logger.info('file available--> ' + filename)
+    else:
+        logger.error(
+            'file does not exists. exit execution--> ' + filename)
+        sys.exit(-1)
 
 def main():
 #     args = get_args(sys.argv[1:])
